bed1e2b8d0430bf54235f48b68b27c726ff76495
Hichu <longdam1312@gmail.com>
2025-09-08 00:25:19 +0700

    * combat

--- Assets/Plugins/KinematicCharacterController/Base/Scripts/Character/CharacterCombat.cs
+++ Assets/Plugins/KinematicCharacterController/Base/Scripts/Character/CharacterCombat.cs
@@ -1,29 +1,70 @@
-using Hichu;
-using Sirenix.OdinInspector;
+﻿using Hichu;
 using UnityEngine;
 
 namespace Kcc.Base
 {
     public class CharacterCombat : MonoBehaviour
     {
-        [Title("Combat Config")]
-        [SerializeField] private float _attackCooldown = 1f;
+        [Header("Stats")]
+        [SerializeField] private int _maxHealth = 100;
+        [SerializeField] private int _currentHealth;
+        [SerializeField] private int _damage = 10;
+
+        [Header("Combat Settings")]
+        [SerializeField] private float _attackSpeed = 1f;
         private float _lastAttackTime;
+        private bool hasDied = false;
+        private void Awake()
+        {
+            _currentHealth = _maxHealth;
+        }
 
         public void Attack(FieldOfView fov)
         {
-            if (Time.time >= _lastAttackTime + _attackCooldown)
+            if (Time.time >= _lastAttackTime + _attackSpeed)
             {
                 _lastAttackTime = Time.time;
 
                 if (fov.visibleTargets.Count == 0 || fov.visibleTargets == null) return;
 
-                LDebug.Log<CharacterCombat>($" {fov.visibleTargets.Count} enemy Get hit");
+                foreach (var target in fov.visibleTargets)
+                {
+                    target.GetComponent<CharacterCombat>().TakeDamage(_damage);
+                }
             }
             else
             {
-                float remain = (_lastAttackTime + _attackCooldown) - Time.time;
+                float remain = (_lastAttackTime + _attackSpeed) - Time.time;
             }
+        }
+
+        public void TakeDamage(int amount)
+        {
+            if (hasDied) return;
+
+            _currentHealth -= amount;
+            _currentHealth = Mathf.Max(_currentHealth, 0);
+
+            if (_currentHealth <= 0)
+            {
+                Die();
+            }
+            else
+            {
+                LDebug.Log<CharacterCombat>($"take {amount} damage");
+            }
+        }
+
+        private void Die()
+        {
+            hasDied = true;
+            LDebug.Log($"[{name}] has died.");
+            // TODO: thêm logic chết (disable, animation, v.v.)
+        }
+
+        public bool IsAlive()
+        {
+            return _currentHealth > 0;
         }
     }
 }
