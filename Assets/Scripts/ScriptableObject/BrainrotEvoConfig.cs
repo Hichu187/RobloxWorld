afe54d3856b67094ae5a944a2cf5a5888ade5c71
Hichu <longdam1312@gmail.com>
2025-09-18 23:52:01 +0700

    * base build

--- Assets/Scripts/ScriptableObject/BrainrotEvoConfig.cs
+++ Assets/Scripts/ScriptableObject/BrainrotEvoConfig.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 using Hichu;
 using Sirenix.OdinInspector;
-using UnityEditor;
 
 namespace Game
 {
@@ -17,20 +16,6 @@ namespace Game
         public float damage = 0;
         public float health;
 
-        [Button]
-        private void SetName()
-        {
-            if (string.IsNullOrWhiteSpace(brainrotName)) return;
-
-            string path = AssetDatabase.GetAssetPath(this);
-            string currentName = System.IO.Path.GetFileNameWithoutExtension(path);
-
-            if (!string.IsNullOrEmpty(path) && currentName != brainrotName)
-            {
-                AssetDatabase.RenameAsset(path, brainrotName);
-                AssetDatabase.SaveAssets();
-            }
-        }
     }
 }
 
