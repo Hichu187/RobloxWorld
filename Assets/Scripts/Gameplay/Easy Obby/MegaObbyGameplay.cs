5be1b3118ec5544b3ae7de91f1f7191bffa04fea
Hichu <longdam1312@gmail.com>
2025-11-09 22:04:53 +0700

    * fix mega

---
+++ Assets/Scripts/Gameplay/Easy Obby/MegaObbyGameplay.cs
@@ -0,0 +1,57 @@
+using Cysharp.Threading.Tasks;
+using Hichu;
+using UnityEngine;
+
+namespace Game
+{
+    public class MegaObbyGameplay : BaseGameplay
+    {
+        public override void Start()
+        {
+            base.Start();
+
+            int curCheckpointIndex = DataAchievement.megaObbyCheckpoint;
+
+            curCheckpoint = checkpoints[curCheckpointIndex];
+            player.character.motor.SetPositionAndRotation(checkpoints[curCheckpointIndex].transform.position, checkpoints[curCheckpointIndex].transform.rotation);
+        }
+
+        protected override void SubscribeEvent()
+        {
+            base.SubscribeEvent();
+
+            StaticBus<Event_Player_Dead>.Subscribe(EventPlayerDead);
+        }
+
+        protected override void UnsubscribeEvent()
+        {
+            base.UnsubscribeEvent();
+
+            StaticBus<Event_Player_Dead>.Unsubscribe(EventPlayerDead);
+        }
+
+        public override async void EventPlayerDead(Event_Player_Dead e)
+        {
+            base.EventPlayerDead(e);
+
+            await UniTask.WaitForSeconds(2f);
+            RespawnCheckpoint();
+        }
+
+        public override void EventCheckpoint(Event_Checkpoint e)
+        {
+            base.EventCheckpoint(e);
+
+            if (curCheckpoint == e.checkpoint) return;
+
+            int checkpointIndex = checkpoints.IndexOf(e.checkpoint);
+
+            if (DataAchievement.megaObbyCheckpoint >= checkpointIndex) return;
+
+            DataAchievement.SetMegaObbyCheckpoint(checkpointIndex);
+
+            curCheckpoint = e.checkpoint;
+            curCheckpoint.PlayFX();
+        }
+    }
+}
