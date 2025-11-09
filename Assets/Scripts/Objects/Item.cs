630241b33829683a1a0ae630169b91c090a32d70
Hichu <longdam1312@gmail.com>
2025-11-09 23:06:52 +0700

    * item

---
+++ Assets/Scripts/Objects/Item.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    public class Item : MonoBehaviour
+    {
+        public ItemConfig config;
+
+        public List<GameObject> models;
+    }
+}
--- Assets/Scripts/ScriptableObject/ItemConfig.cs
+++ Assets/Scripts/ScriptableObject/ItemConfig.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace Game
 {
-    public class WeaponConfig : ScriptableObject
+    public class ItemConfig : ScriptableObject
     {
     }
 }
